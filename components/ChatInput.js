45d3c0551fdd941e413f1024416dd184fadaffd6
ANWESHA2202 <anwesha.sanyal22@gmail.com>
2024-07-21 01:58:01 +0530

    send text message basic

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -1,17 +1,63 @@
 import { TextField } from "@mui/material";
 import styles from "@/modules/MeetChat/meetChat.module.scss";
 import { SendRounded } from "@mui/icons-material";
+import { useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { auth, db } from "@/firebase.config";
+import { useRouter } from "next/router";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export const sendMessage = async (inputText, auth, roomId) => {
+  const messagesRef = collection(db, "messages");
+  await addDoc(messagesRef, {
+    content: inputText,
+    mediaUrl: "",
+    createdAt: serverTimestamp(),
+    user: auth.currentUser.displayName,
+    photoUrl: auth.currentUser.photoURL,
+    roomId: roomId,
+  });
+};
+
 const ChatInput = () => {
+  const router = useRouter();
+  const [inputText, setInputText] = useState("");
+
+  const queryClient = useQueryClient();
+
+  const sendMessageMutation = useMutation({
+    mutationFn: async () => {
+      const roomId = router.query["room-id"];
+      await sendMessage(inputText, auth, roomId);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["messages", router.query["room-id"]]);
+      setInputText("");
+    },
+    onError: (error) => {
+      console.error("Error sending message:", error);
+    },
+  });
+
+  const handleSendMessage = () => {
+    sendMessageMutation.mutate();
+  };
   return (
     <div className={styles.inputContainer}>
       <TextField
         className={styles.inputField}
         multiline
         maxRows={4}
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        // onKeyDown={() => handleSendMessage()}
         placeholder="Type a message"
         InputProps={{
           endAdornment: (
-            <span className={styles.sendButton}>
+            <span
+              className={styles.sendButton}
+              onClick={() => handleSendMessage()}
+            >
               <SendRounded />
             </span>
           ),
