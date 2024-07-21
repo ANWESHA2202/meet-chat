59c13ce71eeebf649913b1eaa587d38ba038ee49
ANWESHA2202 <anwesha.sanyal22@gmail.com>
2024-07-21 17:08:25 +0530

    user info minor styling

--- modules/MeetChat/RoomChat/UserInfo.js
+++ modules/MeetChat/RoomChat/UserInfo.js
@@ -1,5 +1,39 @@
+import ProfileHeader from "@/components/ProfileHeader";
+import { auth } from "@/firebase.config";
+import styles from "@/modules/MeetChat/meetChat.module.scss";
+import { ArrowDropDown } from "@mui/icons-material";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import { useRouter } from "next/router";
+
 const UserInfo = () => {
-  return <div>UserInfo</div>;
+  const router = useRouter();
+  return (
+    <div className={styles.userInfoContainer}>
+      <ProfileHeader />
+      <Accordion className={styles.Accordion} defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ArrowDropDown />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <span>Information</span>
+        </AccordionSummary>
+
+        <AccordionDetails
+          sx={{ borderTop: "1px solid rgba(245, 245, 245, 0.253)" }}
+        >
+          <div>
+            <span>Email: </span>
+            <span>{auth?.currentUser?.email}</span>
+          </div>
+          <div>
+            <span>Room Id: </span>
+            <span>{router?.query?.["room-id"]}</span>
+          </div>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
 };
 
 export default UserInfo;
