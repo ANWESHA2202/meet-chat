9e8f034d1787f0be972098904b85452b44cb27dd
ANWESHA2202 <anwesha.sanyal22@gmail.com>
2024-07-21 13:58:12 +0530

    create room states ui

--- components/room-joiner/CreateRoom.js
+++ components/room-joiner/CreateRoom.js
@@ -2,30 +2,38 @@ import { useMutation } from "@tanstack/react-query";
 import { generateUniqueMeetChatLinkId } from "../utils";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import styles from "@/modules/MeetChat/meetChat.module.scss";
+import { Button, CircularProgress } from "@mui/material";
 
 const CreateRoom = () => {
   const router = useRouter();
-  const [uniqueId, setUniqueId] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const regenerateIdMutation = useMutation({
     mutationFn: generateUniqueMeetChatLinkId,
     onSuccess: (data) => {
-      setUniqueId(data);
       router.push(data);
+      setIsLoading(false);
     },
     onError: (err) => {
       console.log(err);
+      setIsLoading(false);
     },
   });
 
-  const regenerateId = () => {
+  const regenerateId = async () => {
+    setIsLoading(true);
     regenerateIdMutation.mutate();
   };
   return (
-    <>
-      <h1>Your unique Meet-like ID: {uniqueId}</h1>
-      <button onClick={regenerateId}>Generate New ID</button>
-    </>
+    <Button
+      variant="contained"
+      disabled={isLoading}
+      className={styles.createRoom}
+      onClick={regenerateId}
+    >
+      {isLoading ? <CircularProgress /> : "Generate New ID"}
+    </Button>
   );
 };
 
